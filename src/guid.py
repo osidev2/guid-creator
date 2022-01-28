10bef9e9e1e74f355f834ab68544a7156e537886
Michiel Osinga <m.osinga1976@kpnmail.nl>
2022-01-28 15:41:07 +0100

    change to uppercase

--- src/guid.py
+++ src/guid.py
@@ -2,7 +2,7 @@ from time import sleep
 from uuid import uuid4
 
 def create_guid():
-    guid = str(uuid4()).replace('-','')
+    guid = str(uuid4()).replace('-','').upper()
     return guid
 
 while True:
